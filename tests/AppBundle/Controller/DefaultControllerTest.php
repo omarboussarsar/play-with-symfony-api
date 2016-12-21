8ecfdf4926f86345b31d74f579f57c10414284eb
Omar Boussarsar <boussarsar.omar@gmail.com>
2016-12-22 00:34:00 +0100

    Add firstExampleAction in DefaultController Add testFirstExample in DefaultControllerTest

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -15,4 +15,14 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
+    
+    public function testFirstExample()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        //$this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    }
 }
